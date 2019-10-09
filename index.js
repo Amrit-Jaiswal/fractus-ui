29eb17aa12701ee08fbc0384243bd5984c1a3142
Amrit <jaiswal.amrit@gmail.com>
2019-10-09 14:07:13 +0530

    test

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-const http = require("http");
+// import routess from "@/src/routes/routes.js";
 
+// var express = require("express");
+// var app = express();
+
+// var routes = require("./src/routes/routes");
+
+// app.use("/", routess);
+
+const http = require("http");
 const server = http.createServer((request, response) => {
   response.writeHead(200, { "Content-Type": "text/plain" });
   response.end("Hello World!");
+  // server.use("/", routes);
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
