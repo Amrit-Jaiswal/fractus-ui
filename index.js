0c81ef6a3327b0e37da01404bc6055130f11567a
Amrit <jaiswal.amrit@gmail.com>
2019-10-09 17:34:21 +0530

    trying another thing

--- index.js
+++ index.js
@@ -7,4 +7,7 @@ var port = process.env.PORT || 3000;
 
 app.use(express.static("dist"));
 
+app.get("*", (req, res) => {
+  res.sendFile(`index.html`, { root: "dist" });
+});
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
