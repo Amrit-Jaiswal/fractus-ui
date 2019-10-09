4a8240fb2e880f9bb030fb3b66b730953593adbe
Amrit <jaiswal.amrit@gmail.com>
2019-10-09 18:13:52 +0530

    some more

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 // import routess from "@/src/routes/routes.js";
 
 var express = require("express");
+const path = require("path");
 var app = express();
+const resolve = file => path.resolve(__dirname, file);
 var port = process.env.PORT || 3000;
 // var routes = require("./src/routes/routes");
 
-app.use(express.static("dist"));
+const serve = (path, cache) =>
+  express.static(resolve(path), {
+    maxAge: cache ? 1000 * 60 * 60 * 24 * 30 : 0
+  });
+app.use("/", serve("./dist", true));
 
 app.get("*", (req, res) => {
   res.sendFile(`index.html`, { root: "dist" });
