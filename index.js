69aaf43b603d72c52e1bb32c9df3bf74d96e6da8
Amrit <jaiswal.amrit@gmail.com>
2019-10-09 14:20:59 +0530

    node start

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
 // import routess from "@/src/routes/routes.js";
 
-// var express = require("express");
-// var app = express();
-
+var express = require("express");
+var app = express();
+var port = process.env.PORT || 3000;
 // var routes = require("./src/routes/routes");
 
-// app.use("/", routess);
+app.use(express.static("dist"));
 
-const http = require("http");
-const server = http.createServer((request, response) => {
-  response.writeHead(200, { "Content-Type": "text/plain" });
-  response.end("Hello World!");
-  // server.use("/", routes);
-});
-
-const port = process.env.PORT || 3001;
-server.listen(port);
-
-console.log("Server running at http://localhost:%d", port);
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
