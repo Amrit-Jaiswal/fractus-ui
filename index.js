bd316791c2f3ea4709b1f87eff47b97109310352
Amrit <jaiswal.amrit@gmail.com>
2019-10-09 15:25:07 +0530

    dist updated

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ var app = express();
 var port = process.env.PORT || 3000;
 // var routes = require("./src/routes/routes");
 
-app.use(express.static("dist/index.html"));
+app.use(express.static("dist"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
